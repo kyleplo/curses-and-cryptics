d0176f6998a6375a0bce67120b97f1c971be19d1
kyleplo <31634240+kyleplo@users.noreply.github.com>
2025-12-23 20:46:08 -0500

    Initial commit

---
+++ common/src/main/java/com/kyleplo/curses_and_crusades/ExampleMod.java
@@ -0,0 +1,9 @@
+package com.kyleplo.curses_and_crusades;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "curses_and_crusades";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
